d7002d7231b19af4c73129caa41523b9b5266710
Danil Pismenny <danil@brandymint.ru>
2021-05-18 14:48:14 +0300

    minor

--- app/decorators/drainer_decorator.rb
+++ app/decorators/drainer_decorator.rb
@@ -4,6 +4,10 @@ class DrainerDecorator < ApplicationDecorator
   delegate_all
 
   def self.table_columns
-    %i[id class market upstream account upstream_market]
+    %i[id drainer_class market upstream account upstream_market]
+  end
+
+  def drainer_class
+    object.class
   end
 end
