05d3dedeba76e4e7cfd284b91f2cd72b1da91af4
Danil Pismenny <danil@brandymint.ru>
2021-05-19 10:43:42 +0300

    Retry on connection error

--- bin/collector.rb
+++ bin/collector.rb
@@ -7,10 +7,20 @@ SdNotify.ready
 God.instance
 SdNotify.status("God was born!")
 
+SAFE_ERRORS = [ Faraday::ConnectionFailed ]
+
 EM.run do
   God.drainers.each do |drainer|
     God.logger.info "Attach #{drainer}"
     drainer.attach
+  end
+rescue => err
+  if SAFE_ERRORS.include? err
+    God.logger.warn "Catch #{err} retry after 1 second"
+    sleep 1
+    retry
+  else
+    report_exception err
   end
 end
 SdNotify.stopping
