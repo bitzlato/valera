016bf53b3e1f437860f5525d7b65ec214d925e9f
Danil Pismenny <danil@brandymint.ru>
2021-06-22 13:51:12 +0300

    minor

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,4 @@
 Sentry.init do |config|
-  # config.dsn = 'https://9b238dde9c054004947f9d5bb0b636e4@o244676.ingest.sentry.io/5796426'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
