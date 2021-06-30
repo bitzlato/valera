3fdac9d74ec5de48674b48bd6b1aa992de8c9dc4
Danil Pismenny <danil@brandymint.ru>
2021-06-30 13:53:56 +0300

    Disable SSL verification

---
+++ config/initializers/faraday.rb
@@ -0,0 +1,12 @@
+require 'faraday'
+module Faraday
+  class Adapter
+    class NetHttp < Faraday::Adapter
+      # See original definition here:
+      # https://github.com/technoweenie/faraday/blob/412844ac1d90faef1bba5eed25091279358bdf99/lib/faraday/adapter/net_http.rb#L107
+      def ssl_verify_mode(_ssl)
+        OpenSSL::SSL::VERIFY_NONE
+      end
+    end
+  end
+end
