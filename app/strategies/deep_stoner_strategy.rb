11a9e7d7273e7088c01c4939e7d88dcf2bc01238
Danil Pismenny <danil@brandymint.ru>
2021-07-02 10:12:35 +0300

    Disable buyout if strategy is not enabled

--- app/strategies/deep_stoner_strategy.rb
+++ app/strategies/deep_stoner_strategy.rb
@@ -73,11 +73,15 @@ class DeepStonerStrategy < Strategy
 
   def trade_created(trade)
     if settings.buyout_enable?
-      BuyoutOrderCreator
-        .call(trade: trade,
-              buyout_account: buyout_account,
-              ask_percentage: settings.buyout_ask_percentage,
-              bid_percentage: settings.buyout_bid_percentage)
+      if settings.enabled?
+        BuyoutOrderCreator
+          .call(trade: trade,
+                buyout_account: buyout_account,
+                ask_percentage: settings.buyout_ask_percentage,
+                bid_percentage: settings.buyout_bid_percentage)
+      else
+        logger.warn("Strategy is disabled. Skip buyout for trade #{trade.id}..")
+      end
     end
     super
   end
