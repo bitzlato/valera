e97f9e2fdfb0c29283b3bfe1c1074792cfcac105
Danil Pismenny <danil@brandymint.ru>
2021-09-02 18:33:22 +0300

    Remove root_prefix

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/', to: redirect(Settings.root_prefix)
-  scope Settings.root_prefix do
-    root to: 'strategies#index'
-    resources :strategies, only: %i[index show]
-    resources :markets, only: %i[index show]
-    resources :strategy_settings
-    resources :upstream_markets, only: %i[index show]
-    resources :upstreams, only: %i[index show]
-    resources :accounts, only: %i[index show]
-    resources :drainers, only: %i[index show]
-    resources :trades, only: %i[index show]
-    resources :buyout_orders, only: %i[index show]
-  end
+  root to: 'strategies#index'
+  resources :strategies, only: %i[index show]
+  resources :markets, only: %i[index show]
+  resources :strategy_settings
+  resources :upstream_markets, only: %i[index show]
+  resources :upstreams, only: %i[index show]
+  resources :accounts, only: %i[index show]
+  resources :drainers, only: %i[index show]
+  resources :trades, only: %i[index show]
+  resources :buyout_orders, only: %i[index show]
 end
