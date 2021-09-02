c9fb53bcc7fa85f4bb625279fd3de0e8630c91cd
Danil Pismenny <danil@brandymint.ru>
2021-09-02 18:56:13 +0300

    minor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with **Rails.application.credentials.dig(:http_basic_auth) # if Rails.env.production?
+  http_basic_authenticate_with(**Rails.application.credentials.dig(:http_basic_auth))
 
   private
 
