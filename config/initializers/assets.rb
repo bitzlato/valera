dd796bd31e972e644d228646538fdf38dd5c5d41
Danil Pismenny <danil@brandymint.ru>
2021-09-02 18:46:05 +0300

    Fix root_prefix in config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.prefix = "#{Settings.root_prefix}/assets" if Settings.root_prefix.present?
+Rails.application.config.assets.prefix = "#{Settings.root_prefix}/assets" if Settings.key? :root_prefix
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
