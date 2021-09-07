dd82d3e35982b9cd2704e9c7d5ef2745749f5042
Danil Pismenny <danil@brandymint.ru>
2021-09-07 13:33:22 +0300

    strategy class selector

---
+++ app/controllers/concerns/selected_strategy.rb
@@ -0,0 +1,17 @@
+# Copyright (c) 2019 Danil Pismenny <danil@brandymint.ru>
+
+# frozen_string_literal: true
+
+module SelectedStrategy
+  extend ActiveSupport::Concern
+
+  included do
+    helper_method :selected_strategy
+  end
+
+  private
+
+  def selected_strategy
+    params[:strategy_class]
+  end
+end
--- app/controllers/strategies_controller.rb
+++ app/controllers/strategies_controller.rb
@@ -4,11 +4,13 @@
 
 class StrategiesController < ApplicationController
   include SelectedMarket
+  include SelectedStrategy
 
   def index
     strategies = god.strategies
     strategies.each(&:reload)
     strategies = strategies.filter { |s| s.market == selected_market } if selected_market.present?
+    strategies = strategies.filter { |s| s.class.name == selected_strategy } if selected_strategy.present?
     render locals: { strategies: strategies }
   end
 
