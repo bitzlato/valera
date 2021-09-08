5f756a06392099362e00a63e20838466a79bdb30
Danil Pismenny <danil@brandymint.ru>
2021-09-08 15:45:44 +0300

    Add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require best_in_place
 //= require best_in_place.jquery-ui
+//= require moment
 //= require bootstrap
 //= require bootstrap4-datetimepicker/build/js/bootstrap-datetimepicker.min
 //= require_tree .
