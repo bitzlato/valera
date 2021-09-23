87be7c7f50caa4fd946ff5ccd71098e2f6aec030
Danil Pismenny <danil@brandymint.ru>
2021-09-23 16:21:08 +0300

    Fix tests

--- config/initializers/clients.rb
+++ config/initializers/clients.rb
@@ -4,6 +4,7 @@
 
 require 'valera/peatio_client'
 require 'valera/binance_client'
+require 'valera/heco_client'
 require 'valera/monolithos_client'
 require 'peatio/client/websocket'
 require 'huobi/websocket'
---
+++ lib/valera/heco_client.rb
@@ -0,0 +1,8 @@
+# Copyright (c) 2021 Danil Pismenny <danil@brandymint.ru>
+
+require_relative 'base_client'
+
+module Valera
+  class HecoClient < BaseClient
+  end
+end
