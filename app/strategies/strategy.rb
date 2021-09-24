0e30dd5b412d04404681a389e3a7b8994ab7b649
Danil Pismenny <danil@brandymint.ru>
2021-09-24 12:45:09 +0300

    Add logger into Strategy

--- app/strategies/strategy.rb
+++ app/strategies/strategy.rb
@@ -51,7 +51,7 @@ class Strategy
     @state = state_class.build id: id
     @comment = comment
     @logger = ActiveSupport::TaggedLogging.new(_build_auto_logger).tagged([self.class, id].join(' '))
-    @updater = OrdersUpdater.new(market: market, account: account, name: name)
+    @updater = OrdersUpdater.new(market: market, account: account, name: name, logger: logger)
     # TODO: customize used upstreams
     @upstreams = Upstream.all
     @upstream_markets = market.upstream_markets
